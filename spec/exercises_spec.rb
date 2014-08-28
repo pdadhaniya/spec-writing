fcc83c193a2859d41b18cf7a69826d0c5c15c665
Parag Dadhaniya <paragdadhaniya@gmail.com>
2014-08-28 14:47:03 -0700

    add test to return nope if .ex0 method is called on 'wishes'

--- spec/exercises_spec.rb
+++ spec/exercises_spec.rb
@@ -9,5 +9,10 @@ describe "Exercises" do
 			result = Exercises.ex0("ha")
 			expect(result).to eq("hahaha")
 		end
+
+		it "should return 'nope' if the input is 'wishes'" do
+			result = Exercises.ex0("wishes")
+			expect(result).to eq("nope")
+		end
 	end
 end
